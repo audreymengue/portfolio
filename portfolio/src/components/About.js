32c1ec9e46be40f6ac23e3ae52fb9a93ad6e5e0f
Audrey Mengue <massoumeharmonie@gmail.com>
2022-02-14 22:01:43 +0000

    First commit

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+import image from "../image.png";
+
+export default function About() {
+        return (
+          <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+              <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                  Hi there and welcome! 
+                  <br className="hidden lg:inline-block" />I'm Audrey. A software engineer.
+                </h1>
+                <p className="mb-8 leading-relaxed">
+                I am based in Accra, Ghana and hold a Bsc in Information Technology as well as <br/>  a certification in Microsoft Azure. 
+                During my spare time, I volunteer with local and <br/>  international communities as a speaker.
+                </p>
+                <div className="flex justify-center">
+                  <a
+                    href="#contact"
+                    className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
+                    Hire me today
+                  </a>
+                  <a
+                    href="#projects"
+                    className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                    List of projects
+                  </a>
+                </div>
+              </div>
+              <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                <img
+                  className="object-cover object-center rounded"
+                  alt="hero"
+                  src={image}
+                />
+              </div>
+            </div>
+          </section>
+        );
+}
---
+++ portfolio/src/components/Skills.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
+
+export default function Skills() {
+  return (
+    <section id="skills" className="bg-slate-300">
+    <div className="container px-5 py-10 mx-auto">
+      <div className="text-center mb-20">
+        <ChipIcon className="w-10 inline-block mb-4" />
+        <h1 className="sm:text-4xl text-3xl font-medium title-font text-slate-900 mb-4">
+          Skills &amp; Technologies
+        </h1>
+        <p className="text-slate-600 leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          For the past year, I have been working on projects that leverage the technologies below.
+        </p>
+      </div>
+      <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2 items-center">
+        
+          <div className="p-2 sm:w-1/2 w-full">
+            <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+              <BadgeCheckIcon className="text-blue-400 w-6 h-6 flex-shrink-0 mr-4" />
+              <span className="title-font font-medium text-white">
+                <p>SpringBoot </p>
+              </span>
+            </div>
+          </div>
+        
+            
+
+      
+        
+          <div className="p-2 sm:w-1/2 w-full">
+          <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+              <BadgeCheckIcon className="text-blue-400 w-6 h-6 flex-shrink-0 mr-4" />
+              <span className="title-font font-medium text-white">
+                <p>React</p>
+              </span>
+            </div>
+          </div>
+        
+      
+
+      
+          <div className="p-2 sm:w-1/2 w-full">
+          <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+              <BadgeCheckIcon className="text-blue-400 w-6 h-6 flex-shrink-0 mr-4" />
+              <span className="title-font font-medium text-white">
+                <p>Azure </p>
+              </span>
+            </div>
+          </div>
+        
+      
+
+      
+        
+          <div className="p-2 sm:w-1/2 w-full">
+          <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+              <BadgeCheckIcon className="text-blue-400 w-6 h-6 flex-shrink-0 mr-4" />
+              <span className="title-font font-medium text-white">
+                <p>Tailwinds </p>
+              </span>
+            </div>
+          </div>
+
+          </div>
+
+        
+        
+      
+    </div>
+  </section>
+  );
+}
